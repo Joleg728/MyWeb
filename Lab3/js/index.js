d6fd100a150a24e2372de34df756c5aadf7c9670
Oleg <zoleg941@gmail.com>
2025-03-04 22:25:19 +0300

    edit 04.03.2025 22:25

--- Lab3/js/index.js
+++ Lab3/js/index.js
@@ -1,17 +1,38 @@
+
+/*MyUgly.volume = 0.1;
+
+MyUgly.loop = true*/
+
 function myFunc()
 {
-    const sound = new Audio("assets/audio/Still Alive.mp3");
+    var Still_audio = document.getElementById("StillAudio");
 
-    sound.play();
+    Still_audio.loop = true;
 
-    sound.loop();
+    //const sound = new Audio("assets/audio/Still Alive.mp3");
+
+    //sound.play();
+
+    //sound.loop = true;
+
+    return Still_audio.paused ? Still_audio.play() : Still_audio.pause();
 }
 
 function ugly()
 {
-    const ug_ly = new Audio("assets/audio/ugly.mp3");
+    var MyUgly = document.getElementById("UglyAudio");
+
+    MyUgly.volume = 0.1;
+
+    MyUgly.loop = true;
+
+    /*const ug_ly = new Audio("assets/audio/ugly.mp3");
 
     ug_ly.volume = 0.1;
 
-    ug_ly.play();
+    ug_ly.loop = true;*/
+
+    return MyUgly.paused ? MyUgly.play() : MyUgly.pause();
+
+    //ug_ly.play();
 }
