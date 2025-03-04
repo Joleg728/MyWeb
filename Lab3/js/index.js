68b998da869b07544ab8dad203ed464805d6a155
Oleg <zoleg941@gmail.com>
2025-03-04 21:50:12 +0300

    Add 04.03.2025 21:50

---
+++ Lab3/js/index.js
@@ -0,0 +1,17 @@
+function myFunc()
+{
+    const sound = new Audio("assets/audio/Still Alive.mp3");
+
+    sound.play();
+
+    sound.loop();
+}
+
+function ugly()
+{
+    const ug_ly = new Audio("assets/audio/ugly.mp3");
+
+    ug_ly.volume = 0.1;
+
+    ug_ly.play();
+}
