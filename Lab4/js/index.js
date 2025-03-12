088083758d8cdc50de7663a850bf4ab943f610e2
Oleg <zoleg941@gmail.com>
2025-03-12 17:31:57 +0300

    edit 12.03.2025 17:31

--- Lab4/js/index.js
+++ Lab4/js/index.js
@@ -22,24 +22,140 @@ let rews_array = [rew01, rew02, rew03];
 
 let ReviewsNode = document.querySelector("#Reviews");
 
-//window.onload = reviews_load();
-
-//const load_func = 
-
 function range_change()
 {
     let value = document.getElementById("range_id_001").value;
-    //alert(value);
     document.getElementById("range_val_001").innerText = value + "☆";
 }
-
-function reviews_load()
+function range_change_filt()
 {
-    //alert("aaaaaaaaaaa");
+    let value = document.getElementById("range_id_002").value;
+    document.getElementById("range_val_002").innerText = value + "☆";
 
-    rews_array.forEach(element => {
+    if(fltrOnOff_state)
+    {
+        let button = document.getElementById("fltr_btn");
+
+        let fltr_rate = document.getElementById("range_id_002").value;
+
+        del_all_rews(fltrd_numb)
+
+        fltrd_numb = 0;
+
+        rews_array.forEach(element => {
+            if (element.rate == fltr_rate)
+            {
+                one_rew_load(element);
+                fltrd_numb++;
+            }
+        });
+    }
+}
+
+let fltrOnOff_state = false
+
+let fltrd_numb = 0;
+
+function fltrOnOff()
+{
+    let button = document.getElementById("fltr_btn");
+
+    let fltr_rate = document.getElementById("range_id_002").value;
+
+    if (fltrOnOff_state == false)
+    {
+        button.innerText = "Выкл";
+
+        del_all_rews(rews_array.length)
+
+        rews_array.forEach(element => {
+            if (element.rate == fltr_rate)
+            {
+                one_rew_load(element);
+                fltrd_numb++;
+            }
+        });
+
+        fltrOnOff_state = true;
+    } 
+    else
+    {
+        del_all_rews(fltrd_numb)
+            
+        fltrd_numb = 0;
+
+        button.innerText = "Вкл";
+
+        let temp_rews = rews_array.slice(0);
+
+        if (last_button == "good")
+        {
+            temp_rews = temp_rews.sort((a, b) => - a.rate + b.rate)
+        }
+        else if (last_button == "bad")
+        {
+            temp_rews = temp_rews.sort((a, b) => a.rate - b.rate)
+        }
+
+        reviews_load(temp_rews);
+
+        fltrOnOff_state = false;
+
+
+    }
+}
+
+function one_rew_load(element)
+{
+    let sect001 = document.createElement("section");
+    sect001.className = "rew_subblock_1";
+    sect001.id = "REW";
+
+    let sect002 = document.createElement("section");
+    sect002.className = "rew_subblock_2";
+
+    let DOM_img001 = document.createElement("img");
+    DOM_img001.src = element.img_src;
+    DOM_img001.className = "rew_ico_1";
+    DOM_img001.alt = "Ico"
+
+    let p001 = document.createElement("p");
+    p001.className = "rew_name_1";
+    p001.innerText = element.name;
+
+    let p002 = document.createElement("p");
+    p002.className = "rew_text_1";
+    p002.innerText = element.text;
+
+    let p003 = document.createElement("p");
+    p003.className = "rate_text_01";
+    p003.innerText = element.rate + "☆";
+
+    let sect003 = document.createElement("section");
+    
+    sect003.className = "rate_block_01";
+
+    //sect002.textContent = element.name;
+
+    sect003.appendChild(p003);
+
+    //sect001.textContent = element.name;
+    sect002.appendChild(DOM_img001);
+    sect002.appendChild(p001);
+    
+    sect001.appendChild(sect002);
+    sect001.appendChild(p002);
+    sect001.appendChild(sect003);
+
+    ReviewsNode.appendChild(sect001);
+}
+
+function reviews_load( rew_mass )
+{
+    rew_mass.forEach(element => {
         let sect001 = document.createElement("section");
         sect001.className = "rew_subblock_1";
+        sect001.id = "REW";
 
         let sect002 = document.createElement("section");
         sect002.className = "rew_subblock_2";
@@ -47,6 +163,7 @@ function reviews_load()
         let DOM_img001 = document.createElement("img");
         DOM_img001.src = element.img_src;
         DOM_img001.className = "rew_ico_1";
+        DOM_img001.alt = "Ico"
 
         let p001 = document.createElement("p");
         p001.className = "rew_name_1";
@@ -56,7 +173,17 @@ function reviews_load()
         p002.className = "rew_text_1";
         p002.innerText = element.text;
 
+        let p003 = document.createElement("p");
+        p003.className = "rate_text_01";
+        p003.innerText = element.rate + "☆";
+
+        let sect003 = document.createElement("section");
+        
+        sect003.className = "rate_block_01";
+
         //sect002.textContent = element.name;
+
+        sect003.appendChild(p003);
 
         //sect001.textContent = element.name;
         sect002.appendChild(DOM_img001);
@@ -64,70 +191,56 @@ function reviews_load()
         
         sect001.appendChild(sect002);
         sect001.appendChild(p002);
+        sect001.appendChild(sect003);
 
         ReviewsNode.appendChild(sect001);
 
     });
-
-    //alert("s");
-}
-
-async function openFilePicker()
-{
-    let name = "Say my name";
-
-    let value = "Heisenberg";
-
-    document.cookie = "Saymyname=Heisenberg;";
-
-    try
-    {
-        const [fileHandle] = await window.showOpenFilePicker(); // Мы далеко отстали от старомодных файловых менеджеров
-    }
-    catch (error)
-    { // Будьте готовы к неожиданностям и возможным ошибкам пользователей
-        alert("ААААААААААААА ОШИБКА СТОП 0000000000000000000000000000000000000000000000000000");
-    }
-
-    //const type = file.type.replace(/\/.+/, '')
-
-    //const fileData = await fileHandle.getFile();
-
-    //if ( )
-
-    alert(fileHandle);
-
-    //alert(document.cookie);
 }
 
 function custom_review_load()
 {
+    let len = rews_array.length - 1;
+
     let sect001 = document.createElement("section");
     sect001.className = "rew_subblock_1";
+    sect001.id = "REW";
 
     let sect002 = document.createElement("section");
     sect002.className = "rew_subblock_2";
 
     let DOM_img001 = document.createElement("img");
-    DOM_img001.src = custom_rew01.img_src;
+    //DOM_img001.src = custom_rew01.img_src;
+    DOM_img001.src = rews_array[len].img_src;
     DOM_img001.className = "rew_ico_1";
+    DOM_img001.alt = "Ico"
 
     let p001 = document.createElement("p");
     p001.className = "rew_name_1";
-    p001.innerText = custom_rew01.name;
+    //p001.innerText = custom_rew01.name;
+    p001.innerText = rews_array[len].name;
 
     let p002 = document.createElement("p");
     p002.className = "rew_text_1";
-    p002.innerText = custom_rew01.text;
+    //p002.innerText = custom_rew01.text;
+    p002.innerText = rews_array[len].text;
 
-    //sect002.textContent = element.name;
+    let p003 = document.createElement("p");
+    p003.className = "rate_text_01";
+    p003.innerText = rews_array[len].rate + "☆";
 
-    //sect001.textContent = element.name;
+    let sect003 = document.createElement("section");
+        
+    sect003.className = "rate_block_01";
+
+    sect003.appendChild(p003);
+
     sect002.appendChild(DOM_img001);
     sect002.appendChild(p001);
     
     sect001.appendChild(sect002);
     sect001.appendChild(p002);
+    sect001.appendChild(sect003);
 
     ReviewsNode.appendChild(sect001);
 }
@@ -154,10 +267,13 @@ function create_rew()
         custom_rew01.name = name_text;
         custom_rew01.text = rew_text;
         custom_rew01.rate = rate_01;
-        custom_review_load();
+        rews_array.push(custom_rew01);
+        if(!fltrOnOff_state && last_button == "none")
+        {
+            custom_review_load();
+            last_rews_numb = rews_array.length;
+        }
     }
-
-    //alert(name_text + rew_text + rate_01);
 }
 
 let theme_check = false;
@@ -241,4 +357,99 @@ function theme_change()
 
         theme_check = false;
     }
+}
+
+let last_button = "none";
+
+let last_rews_numb = rews_array.length;
+
+function sort_good()
+{
+    if (last_button == "good" || fltrOnOff_state)
+    {
+        return;
+    }
+
+    del_all_rews(last_rews_numb);
+
+    last_rews_numb = 0;
+
+    let sort_rews_array = rews_array.slice(0);
+    
+    sort_rews_array = sort_rews_array.sort((a, b) => - a.rate + b.rate)
+
+    sort_rews_array.forEach(element => {
+        one_rew_load(element);
+        last_rews_numb++;
+    });
+
+    last_button = "good";
+}
+
+function sort_bad()
+{
+    if (last_button == "bad")
+        {
+            return;
+        }
+    
+        del_all_rews(last_rews_numb);
+
+        last_rews_numb = 0;
+    
+        let sort_rews_array = rews_array.slice(0);
+        sort_rews_array = sort_rews_array.sort((a, b) => a.rate - b.rate)
+    
+        sort_rews_array.forEach(element => {
+            one_rew_load(element);
+            last_rews_numb++;
+        });
+    
+        last_button = "bad";
+}
+
+function sort_none()
+{
+    if (last_button == "none")
+        {
+            return;
+        }
+    
+        del_all_rews(last_rews_numb);
+
+        last_rews_numb = rews_array.length;
+    
+        rews_array.forEach(element => {
+            one_rew_load(element);
+        });
+    
+        last_button = "none";
+}
+
+function del_all_rews( count )
+{
+    for (let i = 0; i < count; i++)
+        {
+            var el = document.getElementById("REW");
+            el.remove();
+        }
+}
+
+function readURL()
+{
+    let file01 = document.getElementById('img_inp');
+
+    const flrd = new FileReader();
+
+    flrd.readAsDataURL(file01.files[0]);
+
+    flrd.addEventListener('load', () => {
+
+        const url = flrd.result;
+        
+        const img = new Image();
+
+        custom_rew01.img_src = url;
+
+    });
 }
