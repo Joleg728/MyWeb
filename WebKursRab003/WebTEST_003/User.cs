c77ff2aa6a2385f737665a6a69755b08312abc99
Oleg <zoleg941@gmail.com>
2025-05-25 11:22:05 +0300

    add

---
+++ WebKursRab003/WebTEST_003/User.cs
@@ -0,0 +1,10 @@
+﻿namespace WebTEST_003
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        //public int If { get; set; }
+    }
+}
